a3e776e50ec5fc4ad7488b205fbfd436c2251d95
MuffMuffin <ymakukha@gmail.com>
2022-09-28 18:58:15 +0300

    Things have changed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,42 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+// HTML population:
+const galleryHTML = document.querySelector('.gallery');
+
+const galleryArray = galleryItems.map(({ preview, original, description }) => {
+  return `<a class="gallery__item" href="${original}">
+  <img
+  class="gallery__image"
+  src=""
+  alt="${description}" />
+</a>`;
+});
+
+galleryHTML.innerHTML = galleryArray.join('');
+
+// LazyLoad execution:
+const galleryImgRef = document.querySelectorAll('.gallery img');
+
+const onEntry = observerEntries => {
+  observerEntries.forEach(({ target, isIntersecting }) => {
+    if (isIntersecting) {
+      let source = target.parentNode.href;
+      target.src = source;
+    }
+  });
+};
+
+const observer = new IntersectionObserver(onEntry);
+
+galleryImgRef.forEach(element => observer.observe(element));
+
+// Lightbox generation:
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+gallery.on('show.simplelightbox');
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,65 @@
+import Player from '@vimeo/player';
+import _ from 'lodash';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+let playerPosition = 0;
+
+player
+  .setCurrentTime(30.456)
+  .then(function (seconds) {
+    console.log(seconds);
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
+
+console.log(`Local cookie time: ${document.cookie}`);
+
+const timeLog = ({ seconds }) => {
+  playerPosition = seconds;
+  document.cookie = `${playerPosition}`;
+  // console.log(playerPosition);
+  console.log(document.cookie);
+};
+
+player.on('timeupdate', _.throttle(timeLog, 1000));
+
+// _.throttle(() => {
+//   console.log(localStorage);
+// }, 10000);
+
+// ---
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.getDuration().then(duration => {
+  console.log(duration);
+});
+
+// player.on('timeupdate', ({ seconds }) => {
+//   _.throttle(console.dir(seconds), 10000);
+// });
+
+// document.addEventListener(
+//   'keydown',
+//   _.throttle(event => {
+//     console.log(event.key);
+//   }, 1000)
+// );
